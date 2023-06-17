59a7a3a930d3298cc0b122efbf37df28ac0ba13c
Shohruh88 <shohruh.8899@mail.ru>
2023-06-18 01:18:07 +0500

    header completed

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -41,7 +41,7 @@
         <div class="container my-5 py-5 px-lg-5">
             <div class="row g-5 py-5">
                 <div class="col-lg-6 text-center text-lg-start">
-                    <h1 class="text-white mb-4 animated zoomIn">Biz {{__('lang.salom')}} bilan o'z biznesingizni yanada rivojlantiring</h1>
+                    <h1 class="text-white mb-4 animated zoomIn">Biz bilan o'z biznesingizni yanada rivojlantiring</h1>
                     <p class="text-white pb-3 animated zoomIn">Professional jamoamiz bunda sizga eng mukammal yechimlarni taklif qiladi!</p>
                     <!-- <a href="" class="btn btn-light py-sm-3 px-sm-5 rounded-pill me-3 animated slideInLeft">Free Quote</a>
                     <a href="" class="btn btn-outline-light py-sm-3 px-sm-5 rounded-pill animated slideInRight">Contact Us</a> -->
